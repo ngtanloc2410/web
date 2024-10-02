e46739c28e83c2b5d2b3ae1101fc795794052cba
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-10-02 12:21:17 +0700

    Update hehe1.sh

--- hehe1.sh
+++ hehe1.sh
@@ -2,7 +2,7 @@
 
 # wget -qO- https://raw.githubusercontent.com/ngxson/public-assets/main/install-windows-gcp.sh | sudo bash
 
-IMAGE_URL="https://pub-72ffeee9187442f696dd6c77c2b6f45d.r2.dev/Windows2012r2.gz"
+IMAGE_URL="https://huggingface.co/ngtanloc/test/blob/main/Windows2012r2.gz"
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
