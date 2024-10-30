1841ffdac688c26363761ade0a7b70fe9e6cf40a
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-10-30 16:03:50 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,35 @@
-#!/bin/bash
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 sudo apt update -y && sudo apt upgrade -y
-# Get the IP addresses from the command output
-ip_addresses=$(ip addr show enX0 | awk '/inet / {print $2}' | cut -d'/' -f1)
-i=3
-# Print the IP addresses
-echo "IP Addresses:"
-for ip in $ip_addresses; do
-  sudo docker network create my_network_$i --driver bridge --subnet 192.168.3$i.0/24
-  sudo iptables -t nat -I POSTROUTING -s 192.168.3$i.0/24 -j SNAT --to-source $ip
-  sudo docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k= && sudo docker run -d --network my_network_$i --name repocket_$i -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da -d --restart=always repocket/repocket
-  i=$((i + 1))
-done
+if ip link show enX0 > /dev/null 2>&1; then
+  # Get IP addresses for enX0
+  ip_addresses=$(ip addr show enX0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+  i=3
+
+  # Print the IP addresses and execute Docker commands
+  echo "IP Addresses:"
+  for ip in $ip_addresses; do
+    sudo docker network create my_network_$i --driver bridge --subnet 192.168.3$i.0/24
+    sudo iptables -t nat -I POSTROUTING -s 192.168.3$i.0/24 -j SNAT --to-source $ip
+    sudo docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k=
+    sudo docker run -d --network my_network_$i --name repocket_$i -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da --restart=always repocket/repocket
+    i=$((i + 1))
+  done
+else
+  # Get IP addresses for ens5
+  ip_addresses=$(ip addr show ens5 | awk '/inet / {print $2}' | cut -d'/' -f1)
+  i=3
+
+  # Print the IP addresses and execute Docker commands
+  echo "IP Addresses:"
+  for ip in $ip_addresses; do
+    sudo docker network create my_network_$i --driver bridge --subnet 192.168.3$i.0/24
+    sudo iptables -t nat -I POSTROUTING -s 192.168.3$i.0/24 -j SNAT --to-source $ip
+    sudo docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k=
+    sudo docker run -d --network my_network_$i --name repocket_$i -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da --restart=always repocket/repocket
+    i=$((i + 1))
+  done
+fi
