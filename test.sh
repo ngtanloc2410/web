20bec86099699a32fa5b306e679e0f37446d3765
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-10-30 11:51:56 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Get the IP addresses from the `ip addr` command
-ip_addresses=($(ip addr show enX0 | awk '/inet / {print $2}' | cut -d'/' -f1))
+# Get the IP addresses from the command output
+ip_addresses=$(ip addr show enX0 | awk '/inet / {print $2}' | cut -d'/' -f1)
 
-# Iterate through each IP address
-for ip_address in "${ip_addresses[@]}"; do
-  echo "IP address: $ip_address"
-  # You can add your desired actions here, like writing to a file, executing commands, etc.
-  # For example, to write each IP address to a new line in a file named "ip_addresses.txt":
-  echo "$ip_address" >> ip_addresses.txt
+# Convert the string to an array (optional)
+ip_addresses_array=(${ip_addresses// / })  # Split by spaces (if needed)
+
+# Print the IP addresses
+echo "IP Addresses:"
+for ip in "${ip_addresses[@]}"; do
+  echo "- $ip"
 done
