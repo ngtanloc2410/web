48217d59db64e71d8a354642a528b441ef76e752
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-10-30 11:59:13 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,9 +3,6 @@
 # Get the IP addresses from the command output
 ip_addresses=$(ip addr show enX0 | awk '/inet / {print $2}' | cut -d'/' -f1)
 
-# Convert the string to an array (optional)
-ip_addresses_array=(${ip_addresses// / })  # Split by spaces (if needed)
-
 # Print the IP addresses
 echo "IP Addresses:"
 for ip in "${ip_addresses[@]}"; do
