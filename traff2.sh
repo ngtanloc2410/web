26cefa058f23bb72748ddaf722ba30d6118b3547
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-03 19:55:37 +0700

    Update traff2.sh

--- traff2.sh
+++ traff2.sh
@@ -1,1 +1,13 @@
-tra
+  ip_addresses=$(ip addr show ens5 | awk '/inet6 / {print $2}' | cut -d'/' -f1)
+  i=91
+
+  # Print the IP addresses and execute Docker commands
+  echo "IP Addresses:"
+  for ip in $ip_addresses; do
+    sudo docker network create my_network_$i --driver bridge --subnet 192.168.3$i.0/24
+    sudo iptables -t nat -I POSTROUTING -s 192.168.3$i.0/24 -j SNAT --to-source $ip
+    sudo docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k=
+    sudo docker run -d --network my_network_$i --name repocket_$i -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da --restart=always repocket/repocket
+    #sudo docker run -d --restart unless-stopped --network my_network_$i --name packetshare_$i packetshare/packetshare -accept-tos -email=locpaypal@gmail.com -password=Loc123456789
+    i=$((i + 1))
+  done
