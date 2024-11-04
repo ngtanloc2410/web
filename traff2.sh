bf3083b4595e49e568b58d75fdf7be3bbe506963
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-05 01:11:44 +0700

    Update traff2.sh
    ddfdsfadf

--- traff2.sh
+++ traff2.sh
@@ -2,8 +2,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
-sudo apt update -y && sudo apt upgrade -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 ip=$(curl -4 icanhazip.com)
 echo $ip
 sudo docker run -d --name traff traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k= --device-name $ip
