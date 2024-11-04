600af3f4f08932a8664d80992f432b30679cb799
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-05 01:04:49 +0700

    Update traff2.sh

--- traff2.sh
+++ traff2.sh
@@ -4,6 +4,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 sudo apt update -y && sudo apt upgrade -y
-ip = $(curl -4 icanhazip.com)
+ip=$(curl -4 icanhazip.com)
 sudo docker run -d --name traff traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k= --device-name $ip
 sudo docker run -d --name repocket -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da --restart=always repocket/repocket
