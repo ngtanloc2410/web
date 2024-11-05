20333ee7cd8adfe627b9c039da442cdc6a3424fa
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-06 01:27:55 +0700

    Update traff.sh

--- traff.sh
+++ traff.sh
@@ -14,7 +14,7 @@ if ip link show enX0 > /dev/null 2>&1; then
     sudo docker network create my_network_$i --driver bridge --subnet 192.168.3$i.0/24
     sudo iptables -t nat -I POSTROUTING -s 192.168.3$i.0/24 -j SNAT --to-source $ip
     container_id=$(sudo docker run -d --network my_network_$i alpine/curl curl -s -4 icanhazip.com)
-    $ip1=$(sudo docker logs $container_id)
+    ip1=$(sudo docker logs $container_id)
     echo $ip1
     sudo docker run -d --network my_network_$i --name traff_$i traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k= --device-name $ip1
     sudo docker run -d --network my_network_$i --name repocket_$i -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da --restart=always repocket/repocket
@@ -31,7 +31,10 @@ else
   for ip in $ip_addresses; do
     sudo docker network create my_network_$i --driver bridge --subnet 192.168.3$i.0/24
     sudo iptables -t nat -I POSTROUTING -s 192.168.3$i.0/24 -j SNAT --to-source $ip
-    sudo docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k= --device-name $ip
+    container_id=$(sudo docker run -d --network my_network_$i alpine/curl curl -s -4 icanhazip.com)
+    ip1=$(sudo docker logs $container_id)
+    echo $ip1
+    sudo docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k= --device-name $ip1
     sudo docker run -d --network my_network_$i --name repocket_$i -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da --restart=always repocket/repocket
     #sudo docker run -d --restart unless-stopped --network my_network_$i --name packetshare_$i packetshare/packetshare -accept-tos -email=locpaypal@gmail.com -password=Loc123456789
     i=$((i + 1))
