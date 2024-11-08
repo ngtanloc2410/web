24d80ec550ce94c64f3d287f906e7afea40a4f70
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-08 17:15:15 +0700

    Update proxyrack.sh

--- proxyrack.sh
+++ proxyrack.sh
@@ -1,8 +1,8 @@
+list=()
 if ip link show enX0 > /dev/null 2>&1; then
   # Get IP addresses for enX0
   ip_addresses=$(ip addr show enX0 | awk '/inet / {print $2}' | cut -d'/' -f1)
   i=3
-
   # Print the IP addresses and execute Docker commands
   echo "IP Addresses:"
   for ip in $ip_addresses; do
@@ -11,26 +11,14 @@ if ip link show enX0 > /dev/null 2>&1; then
     ip=$(sudo docker logs $container_id)
     sleep 2
     UUID=$(cat /dev/urandom | LC_ALL=C tr -dc 'A-F0-9' | dd bs=1 count=64 2>/dev/null && echo)
-sudo docker run -d --network my_network_$i --name proxyrack_$i --restart always -e UUID="$UUID" proxyrack/pop
-
-for dem in $(seq 1 120)
-do
-clear
-echo "IP Address: $ip"
-echo "UUID: $UUID"
-  echo "Time remaining : $((120 - dem)) seconds"
-sleep 1
-done
-
-echo -e "\nTime's up!"
-curl -X POST https://peer.proxyrack.com/api/device/add -H "Api-Key: SYNVQSYZKHMA9IYBFA95A30OMONZXF3RKPFWDHWL" -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"device_id":"'"$UUID"'","device_name":"'"$ip"'"}'
+    sudo docker run -d --network my_network_$i --name proxyrack_$i --restart always -e UUID="$UUID" proxyrack/pop
+    list=("${list[@]}" "$UUID|$ip")
     i=$((i + 1))
   done
 else
   # Get IP addresses for ens5
   ip_addresses=$(ip addr show ens5 | awk '/inet / {print $2}' | cut -d'/' -f1)
   i=3
-
   # Print the IP addresses and execute Docker commands
   echo "IP Addresses:"
   for ip in $ip_addresses; do
@@ -38,20 +26,23 @@ else
     sleep 2
     ip=$(sudo docker logs $container_id)
     sleep 2
-        UUID=$(cat /dev/urandom | LC_ALL=C tr -dc 'A-F0-9' | dd bs=1 count=64 2>/dev/null && echo)
-sudo docker run -d --network my_network_$i --name proxyrack_$i --restart always -e UUID="$UUID" proxyrack/pop
-
-for dem in $(seq 1 120)
-do
-clear
-echo "IP Address: $ip"
-echo "UUID: $UUID"
-  echo "Time remaining : $((120 - dem)) seconds"
-sleep 1
-done
-
-echo -e "\nTime's up!"
-curl -X POST https://peer.proxyrack.com/api/device/add -H "Api-Key: SYNVQSYZKHMA9IYBFA95A30OMONZXF3RKPFWDHWL" -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"device_id":"'"$UUID"'","device_name":"'"$ip"'"}'
+    UUID=$(cat /dev/urandom | LC_ALL=C tr -dc 'A-F0-9' | dd bs=1 count=64 2>/dev/null && echo)
+    sudo docker run -d --network my_network_$i --name proxyrack_$i --restart always -e UUID="$UUID" proxyrack/pop
+    list=("${list[@]}" "$UUID|$ip")
     i=$((i + 1))
   done
 fi
+for dem in $(seq 1 120)
+do
+clear
+echo "Time remaining : $((120 - dem)) seconds"
+sleep 1
+done
+echo "\nTime's up!"
+for i in "${list[@]}"; do
+echo "$i"
+b=${i%|*}
+c=${i##*|}
+curl -X POST https://peer.proxyrack.com/api/device/add -H "Api-Key: NQ9WEFFMDQ8TTUUKDLHMPW6IEXVEBBB8RGDCPIOU" -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"device_id":"'"$b"'","device_name":"'"$c"'"}'
+sleep 30
+done
