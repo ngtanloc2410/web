cec52ecd745f5ed21d0c3461df3fbef18dd3360f
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-08 14:29:35 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,15 +4,13 @@ UUID=$(cat /dev/urandom | LC_ALL=C tr -dc 'A-F0-9' | dd bs=1 count=64 2>/dev/nul
 sudo docker run -d --name proxyrack --restart always -e UUID="$UUID" proxyrack/pop
 ip=$(curl -4 icanhazip.com)
 
-count=120
-until [ $count -le 0 ]
+for i in $(seq 1 120)
 do
 clear
 echo "IP Address: $ip"
 echo "UUID: $UUID"
-echo "Time remaining: $count"
+  echo "Time remaining : $(120 - $i)"
 sleep 1
-((count=count-1))
 done
 
 echo -e "\nTime's up!"
