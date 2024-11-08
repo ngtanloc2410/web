c17aa08f1a724694e77bb675e9798e29c8c1c03d
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-08 13:00:03 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 UUID=$(cat /dev/urandom | LC_ALL=C tr -dc 'A-F0-9' | dd bs=1 count=64 2>/dev/null && echo)
 sudo docker run -d --name proxyrack --restart always -e UUID="$UUID" proxyrack/pop
 ip=$(curl -4 icanhazip.com)
-sleep 60
+clear
+echo $ip
+echo $UUID
+countdown_time=120
+for ((i=countdown_time; i>0; i--)); do
+  echo -ne "Time remaining: $i seconds"
+  sleep 1  # Wait for 1 second
+done
 curl -X POST https://peer.proxyrack.com/api/device/add -H "Api-Key: SYNVQSYZKHMA9IYBFA95A30OMONZXF3RKPFWDHWL" -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"device_id":"'"$UUID"'","device_name":"'"$ip"'"}'
