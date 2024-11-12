5a36c51a3f916192ec2d6a138069f33e9a47a01f
ngtanloc2410 <145835725+ngtanloc2410@users.noreply.github.com>
2024-11-12 19:47:56 +0700

    Update traff3.sh

--- traff3.sh
+++ traff3.sh
@@ -4,7 +4,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 ip=$(curl -4 icanhazip.com)
-UUID=$(curl https://proxyrack-9ibn.onrender.com/)
+UUID=$(curl http://3.133.104.114:3000/?proxyrack=$ip)
 echo $ip
 sudo docker run -d --name traff traffmonetizer/cli_v2 start accept --token cCuCGOWZXNnk9dL5BR+cz1QHbjCdXJnFb8e3a9OAS2k= --device-name $ip
 sudo docker run -d --name repocket -e RP_EMAIL=nguyentanloc180@gmail.com -e RP_API_KEY=8873dd7c-f936-4deb-b128-c15dc54813da --restart=always repocket/repocket
